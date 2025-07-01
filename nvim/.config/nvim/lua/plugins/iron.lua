1147dc12110bf906fedc5e32c70f642e3e534ee6
Adrian Rushing <adrianjrushing@gmail.com>
2025-07-01 18:44:39 -0500

    added tmux

---
+++ nvim/.config/nvim/lua/plugins/iron.lua
@@ -0,0 +1,56 @@
+return {
+	"Vigemus/iron.nvim",
+	config = function()
+		local iron = require("iron.core")
+		local view = require("iron.view")
+		local common = require("iron.fts.common")
+
+		iron.setup({
+			config = {
+				scratch_repl = true,
+				repl_definition = {
+					sh = {
+						command = { "zsh" },
+					},
+					python = {
+						command = { "python3" }, -- or { "ipython", "--no-autoindent" }
+						format = common.bracketed_paste_python,
+						block_dividers = { "# %%", "#%%" },
+					},
+				},
+				repl_filetype = function(_, ft)
+					return ft
+				end,
+				repl_open_cmd = view.bottom(40),
+			},
+			keymaps = {
+				toggle_repl = "<space>rr",
+				restart_repl = "<space>rR",
+				send_motion = "<space>sc",
+				visual_send = "<space>sc",
+				send_file = "<space>sf",
+				send_line = "<space>sl",
+				send_paragraph = "<space>sp",
+				send_until_cursor = "<space>su",
+				send_mark = "<space>sm",
+				send_code_block = "<space>sb",
+				send_code_block_and_move = "<space>sn",
+				mark_motion = "<space>mc",
+				mark_visual = "<space>mc",
+				remove_mark = "<space>md",
+				cr = "<space>s<cr>",
+				interrupt = "<space>s<space>",
+				exit = "<space>sq",
+				clear = "<space>cl",
+			},
+			highlight = {
+				italic = true,
+			},
+			ignore_blank_lines = true,
+		})
+
+		-- Additional keymaps for commands
+		vim.keymap.set("n", "<space>rf", "<cmd>IronFocus<cr>", { desc = "Iron Focus" })
+		vim.keymap.set("n", "<space>rh", "<cmd>IronHide<cr>", { desc = "Iron Hide" })
+	end,
+}
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,28 +1,30 @@
 return {
-  {'nvim-telescope/telescope.nvim',
-        tag = '0.1.8',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-        config = function()
-          local builtin = require("telescope.builtin")
-          vim.keymap.set('n','<C-p>',builtin.find_files,{})
-          vim.keymap.set('n','<leader>fg',builtin.live_grep,{})
-        end
-  },
   {
-    'nvim-telescope/telescope-ui-select.nvim',
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-ui-select.nvim", -- put extension as dependency here
+    },
     config = function()
-    require("telescope").setup {
-      extensions = {
-        ["ui-select"] = {
-          require("telescope.themes").get_dropdown {
-          }
-        }
-      }
-    }
-    require("telescope").load_extension("ui-select")
-    end
+      local telescope = require("telescope")
+
+      -- Setup telescope + extensions
+      telescope.setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+
+      -- Load the extension
+      telescope.load_extension("ui-select")
+
+      -- Setup keymaps
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+    end,
   },
-
-
 }
-
