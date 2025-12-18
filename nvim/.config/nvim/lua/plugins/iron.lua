4c815dbbbb0a9c8bd9b37a88ab224bc3afebe929
Adrian Rushing <adrianjrushing@gmail.com>
2025-12-18 15:28:39 -0600

    update nvim

--- nvim/.config/nvim/lua/plugins/iron.lua
+++ nvim/.config/nvim/lua/plugins/iron.lua
@@ -1,59 +1,59 @@
 return {
-  "Vigemus/iron.nvim",
-  config = function()
-    local iron = require("iron.core")
-    local view = require("iron.view")
-    local common = require("iron.fts.common")
+	"Vigemus/iron.nvim",
+	config = function()
+		local iron = require("iron.core")
+		local view = require("iron.view")
+		local common = require("iron.fts.common")
 
-    iron.setup({
-      config = {
-        scratch_repl = true,
-        repl_definition = {
-          sh = {
-            command = { "bash" },
-          },
-          python = {
-            command = { "ipython", "--no-autoindent" },
-            format = common.bracketed_paste_python,
-            block_dividers = { "# %%", "#%%" },
-          },
-        },
-        repl_filetype = function(_, ft)
-          return ft
-        end,
-        repl_open_cmd = view.bottom(40),
-      },
-      keymaps = {
-        toggle_repl = "<space>rr",
-        restart_repl = "<space>rR",
-        send_motion = "<space>sc",
-        visual_send = "<space>sc",
-        send_file = "<space>sf",
-        send_line = "<space>sl",
-        send_paragraph = "<space>sp",
-        send_until_cursor = "<space>su",
-        send_mark = "<space>sm",
-        send_code_block = "<space>sb",
-        send_code_block_and_move = "<space>sn",
-        mark_motion = "<space>mc",
-        mark_visual = "<space>mc",
-        remove_mark = "<space>md",
-        cr = "<space>s<cr>",
-        interrupt = "<space>s<space>",
-        exit = "<space>sq",
-        clear = "<space>cl",
-      },
-      highlight = {
-        italic = true,
-      },
-      ignore_blank_lines = true,
-    })
+		iron.setup({
+			config = {
+				scratch_repl = true,
+				repl_definition = {
+					sh = {
+						command = { "bash" },
+					},
+					python = {
+						command = { "ipython", "--no-autoindent" },
+						format = common.bracketed_paste_python,
+						block_dividers = { "# %%", "#%%" },
+					},
+				},
+				repl_filetype = function(_, ft)
+					return ft
+				end,
+				repl_open_cmd = view.split.vertical.botright(0.40),
+			},
+			keymaps = {
+				toggle_repl = "<leader>rr",
+				restart_repl = "<leader>rR",
+				send_motion = "<leader>sc",
+				visual_send = "<leader>sc",
+				send_file = "<leader>sf",
+				send_line = "<leader>sl",
+				send_paragraph = "<leader>sp",
+				send_until_cursor = "<leader>su",
+				send_mark = "<leader>sm",
+				send_code_block = "<leader>sb",
+				send_code_block_and_move = "<leader>sn",
+				mark_motion = "<leader>mc",
+				mark_visual = "<leader>mc",
+				remove_mark = "<leader>md",
+				cr = "<leader>s<cr>",
+				interrupt = "<leader>s<leader>",
+				exit = "<leader>sq",
+				clear = "<leader>cl",
+			},
+			highlight = {
+				italic = true,
+			},
+			ignore_blank_lines = true,
+		})
 
-    -- Additional keymaps for commands
-    vim.keymap.set("n", "<space>rf", "<cmd>IronFocus<cr>", { desc = "Iron Focus" })
-    vim.keymap.set("n", "<space>rh", "<cmd>IronHide<cr>", { desc = "Iron Hide" })
-    vim.keymap.set("n", "<space>cl", function()
-      require("iron.core").send(nil, { "\x0c" }) -- Send Ctrl-L to clear
-    end, { desc = "Iron Clear REPL" })
-  end,
+		-- Additional keymaps for commands
+		vim.keymap.set("n", "<leader>rf", "<cmd>IronFocus<cr>", { desc = "Iron Focus" })
+		vim.keymap.set("n", "<leader>rh", "<cmd>IronHide<cr>", { desc = "Iron Hide" })
+		vim.keymap.set("n", "<leader>cl", function()
+			require("iron.core").send(nil, { "\x0c" }) -- Send Ctrl-L to clear
+		end, { desc = "Iron Clear REPL" })
+	end,
 }
