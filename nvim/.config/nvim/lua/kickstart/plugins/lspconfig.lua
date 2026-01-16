d6dd734f7e6b08d19255f4938bc7ced123c89525
Adrian Rushing <adrianjrushing@gmail.com>
2026-01-16 10:21:43 -0600

    add basedpyright lsp

--- nvim/.config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -183,11 +183,12 @@ return {
         -- clangd = {},
         -- gopls = {},
         pyright = {},
+        basedpyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
+        --    https://github.compmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
@@ -244,6 +245,4 @@ return {
       }
     end,
   },
-
-
 }
