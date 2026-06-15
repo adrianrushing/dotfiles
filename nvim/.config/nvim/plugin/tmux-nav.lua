052187ec3ff6e8b051ca0885df5e6f92ffbf049a
Adrian Rushing <adrianjrushing@gmail.com>
2026-06-15 12:34:54 -0500

    update tmux nav

---
+++ nvim/.config/nvim/plugin/tmux-nav.lua
@@ -0,0 +1,7 @@
+vim.pack.add { 'https://github.com/christoomey/vim-tmux-navigator' }
+
+vim.keymap.set('n', '<C-h>', '<cmd>TmuxNavigateLeft<CR>')
+vim.keymap.set('n', '<C-j>', '<cmd>TmuxNavigateDown<CR>')
+vim.keymap.set('n', '<C-k>', '<cmd>TmuxNavigateUp<CR>')
+vim.keymap.set('n', '<C-l>', '<cmd>TmuxNavigateRight<CR>')
+vim.keymap.set('n', '<C-\\>', '<cmd>TmuxNavigatePrevious<CR>')
